3d434ccc27ad9cb7315e5e5eedbc541aaba67166
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-11-20 11:06:01 -0600

    JS basics setup

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,10 @@
 //		Error try and catches 
 //		Submit button or enter 
 
+/*------------------------------------------------------->
+
+<-------------------------------------------------------*/
+
 function select(selector, scope = document) {
   return scope.querySelector(selector);
 }
@@ -28,5 +32,54 @@ function removeClass(element, customClass) {
 
 function getRandomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*------------------------------------------------------->
+
+<-------------------------------------------------------*/
+
+const guessCounter = select('.guess-counter');
+const playerInput = select('.player-input');
+const accuracyReport = select('.accuracy');
+const startBtn = select('.start');
+const restartBtn = select('.restart');
+
+/*------------------------------------------------------->
+
+<-------------------------------------------------------*/
+
+function pickNum() {
+	let compNum = getRandomNumber(1, 100);
+	return compNum;
+}
+
+function getGuess() {
+	let playerGuess= Number(playerInput.value.trim());
+	if (playerGuess !== Nan && playerGuess > 0 && playerGuess < 100) {
+		let playerNum = playerGuess;
+		return playerNum;
+	} else {
+		console.log('Please enter a valid number between 1 and 100');
+	}
+	playerInput.value = '';
+}
+
+function countGuess(){
+
+}
+
+function compareNum() {
+	if(compNum === playerNum) {
+		console.log('You got it!');
+	} else if(compNum > playerNum) {
+		console.log('You guessed less than my number');
+	} else if(compNum < playerNum) {
+		console.log('You guessed more than my number');
+	}
+}
+
+function resetGame() {
+	let playerNum = 0;
+	return playerNum;
 }
 
