dccd78261259f4d27efd6deb0fda25493656e10c
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-11-20 11:40:15 -0600

    js functional

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,5 @@
 'use strict';
-
-
-//		Assign a random value within a certain range to a variable 
-// 		If /else/else for larger, smaller, and correct 
-//		Accepts input and validations 
-//		Validation means: trimmed, it is a number and the number within range 
-//		Error try and catches 
-//		Submit button or enter 
+// Reset button needs adjusting 
 
 /*------------------------------------------------------->
 
@@ -31,14 +24,14 @@ function removeClass(element, customClass) {
 }
 
 function getRandomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /*------------------------------------------------------->
 
 <-------------------------------------------------------*/
 
-const guessCounter = select('.guess-counter');
+const guessCounterDisplay = select('.guess-counter');
 const playerInput = select('.player-input');
 const accuracyReport = select('.accuracy');
 const startBtn = select('.start');
@@ -48,38 +41,77 @@ const restartBtn = select('.restart');
 
 <-------------------------------------------------------*/
 
+let guessCounter = 0;
+let compNum;
+
+/*------------------------------------------------------->
+
+<-------------------------------------------------------*/
+
 function pickNum() {
-	let compNum = getRandomNumber(1, 100);
-	return compNum;
+  compNum = getRandomNumber(1, 100);
+  return compNum;
 }
 
 function getGuess() {
-	let playerGuess= Number(playerInput.value.trim());
-	if (playerGuess !== Nan && playerGuess > 0 && playerGuess < 100) {
-		let playerNum = playerGuess;
-		return playerNum;
-	} else {
-		console.log('Please enter a valid number between 1 and 100');
-	}
-	playerInput.value = '';
+  let playerGuess = Number(playerInput.value.trim());
+  if (!isNaN(playerGuess) && playerGuess > 0 && playerGuess <= 100) {
+    return playerGuess;
+  } else {
+    console.log('Please enter a valid number between 1 and 100');
+    accuracyReport.textContent = 'Invalid input. Enter a number between 1 and 100.';
+  }
 }
 
-function countGuess(){
-
+function countGuess() {
+  guessCounter++;
+  guessCounterDisplay.textContent = `Guesses: ${guessCounter}`;
+  return guessCounter;
 }
 
-function compareNum() {
-	if(compNum === playerNum) {
-		console.log('You got it!');
-	} else if(compNum > playerNum) {
-		console.log('You guessed less than my number');
-	} else if(compNum < playerNum) {
-		console.log('You guessed more than my number');
-	}
+function compareNum(playerNum) {
+  if (compNum === playerNum) {
+    console.log('You got it!');
+    accuracyReport.textContent = 'You got it!';
+  } else if (compNum > playerNum) {
+    console.log('You guessed less than my number');
+    accuracyReport.textContent = 'Too low!';
+  } else {
+    console.log('You guessed more than my number');
+    accuracyReport.textContent = 'Too high!';
+  }
 }
 
 function resetGame() {
-	let playerNum = 0;
-	return playerNum;
+  playerNum = 0;
+  guessCounter = 0;
+  compNum = pickNum();
+  guessCounterDisplay.textContent = `Guesses: ${guessCounter}`;
+  accuracyReport.textContent = '';
+  playerInput.value = '';
 }
 
+/*------------------------------------------------------->
+
+<-------------------------------------------------------*/
+
+listen('click', startBtn, () => {
+  compNum = pickNum();
+  console.log(`Random number picked: ${compNum}`);
+  accuracyReport.textContent = 'Game started! Make your first guess.';
+});
+
+listen('keydown', playerInput, (ev) => {
+  if (ev.key === 'Enter') {
+    const playerNum = getGuess();
+    if (playerNum !== undefined) {
+      countGuess();
+      compareNum(playerNum);
+    }
+  }
+	playerInput.value = '';
+});
+
+listen('click', restartBtn, () => {
+  resetGame();
+});
